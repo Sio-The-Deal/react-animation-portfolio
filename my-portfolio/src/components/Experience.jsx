6fddb762ef8b842d92ff256d5f87d0ade6684f30
changs3 <sio.chang3@mail.dcu.ie>
2023-05-13 13:24:33 +0100

    smaller asteroids

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -11,16 +11,16 @@ export const Experience = () => {
       <Float floatIntensity={1} speed={1.5} rotationIntensity={0.5}>
         <Dog
           rotation-y={Math.PI / 2}
-          scale={[1, 1, 1]}
+          scale={[0.5, 0.5, 0.5]}
           position-y={0.1}
         />
       </Float>
-      <Asteroid opacity={0.5} scale={[0.2, 0.2, 0.2]} position-y={[-2, 1, -3]} />
-      <Asteroid opacity={0.5} scale={[0.2, 0.3, 0.2]} position-y={[1.5, -0.5, -2]} />
-      <Asteroid opacity={0.7} scale={[0.1, 0.1, 0.2]} rotation-y={Math.PI / 9} position={[2, -0.2, -2]} />
-      <Asteroid opacity={0.7} scale={[0.2, 0.2, 0.2]} rotation-y={Math.PI / 9} position={[1, -0.2, -12]} />
-      <Asteroid opacity={0.7} scale={[0.25, 0.25, 0.25]} position={[0, 1, -53]} />
-      <Asteroid opacity={0.7} scale={[0.2, 0.2, 0.2]} position={[0, 1, -100]} />
+      <Asteroid opacity={0.5} scale={[0.1, 0.1, 0.2]} position-y={[-2, 1, -3]} />
+      <Asteroid opacity={0.5} scale={[0.1, 0.1, 0.1]} position-y={[1.5, -0.5, -2]} />
+      <Asteroid opacity={0.2} scale={[0.1, 0.1, 0.025]} rotation-y={Math.PI / 9} position={[2, -0.2, -2]} />
+      <Asteroid opacity={0.2} scale={[0.1, 0.1, 0.1]} rotation-y={Math.PI / 9} position={[1, -0.2, -12]} />
+      <Asteroid opacity={0.2} scale={[0.1, 0.1, 0.1]} position={[0, 1, -53]} />
+      <Asteroid opacity={0.2} scale={[0.1, 0.1, 0.1]} position={[0, 1, -100]} />
 
     </>
   );
