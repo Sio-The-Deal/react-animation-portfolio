cfb3e6c2d271da516b2dda8a6473cd04f97a7659
changs3 <sio.chang3@mail.dcu.ie>
2023-05-13 13:15:37 +0100

    import models without animations

---
+++ my-portfolio/src/components/Background.jsx
@@ -0,0 +1,27 @@
+import { Environment, Sphere } from "@react-three/drei";
+import { LayerMaterial, Gradient } from "lamina";
+import * as THREE from "three";
+
+export const Background = () => {
+    return (
+        <>
+            <Environment preset="sunset" />
+            {/* it is possible to change preset to "dawn" ,"night" or "day" */}
+            <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
+            {/* since we are using environment , we can set the rotation-y={Math.PI / 2} to rotate the sphere 90 degrees */}
+                {/* Math.PI / 2 -> Left ,,, -Math.PI / 2 -> Right */}
+                <LayerMaterial
+                    lighting="physical"
+                    transmission={0}
+                    // {/* transmission is the opacity of the material */}
+                    side={THREE.BackSide}
+                >
+                    <Gradient colorA={"black"} colorB={"white"} axes={"y"} start={0} end={-0.5}/> 
+                    {/* change the gradient from vertical to horizontal by changing the axes prop */}
+                </LayerMaterial>
+            </Sphere>
+        </>
+    );
+};
+
+
--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -1,13 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
+import { Background } from "./Background";
+import { Dog } from "./Dog";
+import { Asteroid } from "./Asteroid";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Background />
+      <Float floatIntensity={1} speed={1.5} rotationIntensity={0.5}>
+        <Dog
+          rotation-y={Math.PI / 2}
+          scale={[1, 1, 1]}
+          position-y={0.1}
+        />
+      </Float>
+      <Asteroid opacity={0.5} scale={[0.2, 0.2, 0.2]} position-y={[-2, 1, -3]} />
+      <Asteroid opacity={0.5} scale={[0.2, 0.3, 0.2]} position-y={[1.5, -0.5, -2]} />
+      <Asteroid opacity={0.7} scale={[0.1, 0.1, 0.2]} rotation-y={Math.PI / 9} position={[2, -0.2, -2]} />
+      <Asteroid opacity={0.7} scale={[0.2, 0.2, 0.2]} rotation-y={Math.PI / 9} position={[1, -0.2, -12]} />
+      <Asteroid opacity={0.7} scale={[0.25, 0.25, 0.25]} position={[0, 1, -53]} />
+      <Asteroid opacity={0.7} scale={[0.2, 0.2, 0.2]} position={[0, 1, -100]} />
+
     </>
   );
 };
