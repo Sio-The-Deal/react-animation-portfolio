8470d9fc02a111ea0d14f0e2fc5779a67749b3d1
changs3 <sio.chang3@mail.dcu.ie>
2023-05-15 15:12:32 +0100

    curve completion

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -1,9 +1,54 @@
-import { Float, OrbitControls } from "@react-three/drei";
+import { Float, Line, OrbitControls, useAnimations } from "@react-three/drei";
 import { Background } from "./Background";
 import { Dogfly } from "./Dogfly";
 import { Asteroid } from "./Asteroid";
+import * as THREE from "three";
+import { useMemo } from "react";
+import { Euler, Group, Vector3 } from "three";
+
+
+
+
+
+const LINE_NB_POINTS = 1800;
 
 export const Experience = () => {
+  
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3([
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 0, -10),
+      new THREE.Vector3(-2, 0, -20),
+      new THREE.Vector3(-3, 0, -30),
+      new THREE.Vector3(0, 0, -40),
+      new THREE.Vector3(5, 0, -50),
+      new THREE.Vector3(7, 0, -60),
+      new THREE.Vector3(5, 0, -70),
+      new THREE.Vector3(0, 0, -80),
+      new THREE.Vector3(0, 0, -90),
+      new THREE.Vector3(0, 0, -100),
+
+    ],
+    false,
+    "chordal",
+    0.5)
+  })
+  
+  const linePoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  },[curve]);
+
+
+
+  // generate a plane based on the curve
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape ;
+  }, [curve]);
+
   return (
     <>
       <OrbitControls />
@@ -17,6 +62,28 @@ export const Experience = () => {
           position-y={0.1}
         />
       </Float>
+      <group position-y={-2}>
+        {/* <Line
+          points={linePoints}
+          color={"white"}
+          opacity={0.7}
+          transparentlineqorth={16}
+        /> */}
+        <mesh>
+          <extrudeGeometry
+          args={[
+            shape,
+            {
+              steps: LINE_NB_POINTS,
+              bevelEnabled: false,
+              extrudePath: curve,
+            },
+          ]} />
+          <meshStandardMaterial color={"white"} opacity={0.7} transparent/>
+        </mesh>
+      </group>
+      {/* see doc https://threejs.org/docs/#api/en/geometries/ExtrudeGeometry */}
+
       <Asteroid opacity={0.5} scale={[0.1, 0.1, 0.2]} position-y={[-2, 1, -3]} />
       <Asteroid opacity={0.5} scale={[0.1, 0.1, 0.1]} position-y={[1.5, -0.5, -2]} />
       <Asteroid opacity={0.2} scale={[0.1, 0.1, 0.025]} rotation-y={Math.PI / 9} position={[2, -0.2, -2]} />
